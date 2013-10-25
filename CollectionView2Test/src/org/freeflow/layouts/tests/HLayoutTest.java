04c95ee7ea9e1e6c93ad768867e3b31e9ef4ac85
Richard Ratmansky <rratmansky@mportal.com>
2013-10-25 11:24:26 -0400

    Updated with more tests...

--- CollectionView2Test/src/org/freeflow/layouts/tests/HLayoutTest.java
+++ CollectionView2Test/src/org/freeflow/layouts/tests/HLayoutTest.java
@@ -27,57 +27,83 @@ public class HLayoutTest extends TestCase {
 		super.tearDown();
 	}
 
-	public void testViewPortCorrectFrames1() {
-		layout.setItemWidth(100);
-		layout.setDimensions(500, 500);
+	private SparseArray<FrameDescriptor> prepareLayout(int itemWidth, int width, int height, int viewportX) {
+		layout.setItemWidth(itemWidth);
+		layout.setDimensions(width, height);
 		layout.generateFrameDescriptors();
-		SparseArray<FrameDescriptor> descs = layout.getFrameDescriptors(100, 0);
+		return layout.getFrameDescriptors(100, 0);
+	}
 
+	public void testViewPortCorrectIndex1() {
+		SparseArray<FrameDescriptor> descs = prepareLayout(100, 500, 500, 100);
 		assertNotNull(descs);
 		assertEquals(1, descs.get(descs.keyAt(0)).itemIndex);
-		FrameDescriptor desc = descs.get(descs.keyAt(0));
-		assertEquals(0, desc.frame.left);
-
 		assertEquals(5, descs.size());
 
 		descs = layout.getFrameDescriptors(500, 0);
 		assertNotNull(descs);
 		assertEquals(5, descs.get(descs.keyAt(0)).itemIndex);
-		desc = descs.get(descs.keyAt(0));
-		assertEquals(0, desc.frame.left);
 
 		descs = layout.getFrameDescriptors(550, 0);
 		assertNotNull(descs);
 		assertEquals(5, descs.get(descs.keyAt(0)).itemIndex);
-		desc = descs.get(descs.keyAt(0));
-		assertEquals(-50, desc.frame.left);
 
 	}
 
-	public void testViewPortCorrectFrames2() {
-		layout.setItemWidth(50);
-		layout.setDimensions(500, 500);
-		layout.generateFrameDescriptors();
-		SparseArray<FrameDescriptor> descs = layout.getFrameDescriptors(100, 0);
+	public void testViewPortCorrectFraming1() {
+		SparseArray<FrameDescriptor> descs = prepareLayout(100, 500, 500, 100);
+		assertNotNull(descs);
+
+		FrameDescriptor desc = descs.get(descs.keyAt(0));
+		assertEquals(0, desc.frame.left);
+		assertEquals(0, desc.frame.top);
+		assertEquals(100, desc.frame.width);
+		assertEquals(500, desc.frame.height);
+
+		descs = layout.getFrameDescriptors(550, 0);
+		desc = descs.get(descs.keyAt(0));
+		assertEquals(-50, desc.frame.left);
+		assertEquals(0, desc.frame.top);
+		assertEquals(100, desc.frame.width);
+		assertEquals(500, desc.frame.height);
+
+	}
+
+	public void testViewPortCorrectIndex2() {
+		SparseArray<FrameDescriptor> descs = prepareLayout(50, 500, 500, 100);
+		assertNotNull(descs);
 
 		assertNotNull(descs);
 		assertEquals(2, descs.get(descs.keyAt(0)).itemIndex);
-		FrameDescriptor desc = descs.get(descs.keyAt(0));
-		assertEquals(0, desc.frame.left);
-
 		assertEquals(10, descs.size());
 
 		descs = layout.getFrameDescriptors(500, 0);
 		assertNotNull(descs);
 		assertEquals(10, descs.get(descs.keyAt(0)).itemIndex);
-		desc = descs.get(descs.keyAt(0));
-		assertEquals(0, desc.frame.left);
-
+		
 		descs = layout.getFrameDescriptors(25, 0);
 		assertNotNull(descs);
 		assertEquals(0, descs.get(descs.keyAt(0)).itemIndex);
+
+	}
+
+	public void testViewPortCorrectFraming2() {
+		SparseArray<FrameDescriptor> descs = prepareLayout(50, 500, 500, 100);
+		assertNotNull(descs);
+
+		FrameDescriptor desc = descs.get(descs.keyAt(0));
+		assertEquals(0, desc.frame.left);
+		assertEquals(0, desc.frame.top);
+		assertEquals(50, desc.frame.width);
+		assertEquals(500, desc.frame.height);
+
+		descs = layout.getFrameDescriptors(525, 0);
 		desc = descs.get(descs.keyAt(0));
 		assertEquals(-25, desc.frame.left);
+		assertEquals(0, desc.frame.top);
+		assertEquals(50, desc.frame.width);
+		assertEquals(500, desc.frame.height);
+
 	}
 
 	BaseAdapter itemAdapter = new BaseAdapter() {
