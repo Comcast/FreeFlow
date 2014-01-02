8b99b4e028690a53fbaf27804299c4c564439a07
Arpit Mathur <mathur.arpit@gmail.com>
2014-01-02 15:42:43 -0500

    Updates to test projects.

--- CollectionViews2/src/org/freeflow/collectionviews2/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package org.freeflow.collectionviews2;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-	
-	
-}
---
+++ FreeFlowTestsStub/src/org/freeflow/teststub/MainActivity.java
@@ -0,0 +1,8 @@
+package org.freeflow.teststub;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	
+	
+}
