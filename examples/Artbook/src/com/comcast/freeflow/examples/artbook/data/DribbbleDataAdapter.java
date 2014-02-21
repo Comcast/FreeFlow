7a02cc47cfa5312f5278f86dcee2834ff7459688
Arpit Mathur <mathur.arpit@gmail.com>
2014-02-21 17:01:36 -0500

    Small change to example project to allow just colored boxes or images.

--- examples/Artbook/src/com/comcast/freeflow/examples/artbook/data/DribbbleDataAdapter.java
+++ examples/Artbook/src/com/comcast/freeflow/examples/artbook/data/DribbbleDataAdapter.java
@@ -19,12 +19,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class DribbbleDataAdapter implements SectionedAdapter {
-	
+
 	private Context context;
 	private DribbbleFeed feed;
 	private Section section;
+
+	private int[] colors = new int[] { 0xcc152431, 0xff264C58, 0xffF5C543,
+			0xffE0952C, 0xff9A5325, 0xaaE0952C, 0xaa9A5325, 0xaa152431,
+			0xaa264C58, 0xaaF5C543, 0x44264C58, 0x44F5C543, 0x44152431 };
 	
-	public DribbbleDataAdapter(Context context, DribbbleFeed feed){
+	private boolean hideImages = false;
+
+	public DribbbleDataAdapter(Context context, DribbbleFeed feed) {
 		this.feed = feed;
 		this.context = context;
 		section = new Section();
@@ -34,17 +40,27 @@ public class DribbbleDataAdapter implements SectionedAdapter {
 
 	@Override
 	public long getItemId(int section, int position) {
-		return section*1000+position;
+		return section * 1000 + position;
 	}
 
 	@Override
 	public View getItemView(int section, int position, View convertView,
 			ViewGroup parent) {
-		if(convertView == null){
-			convertView = LayoutInflater.from(context).inflate(R.layout.pic_view, parent, false);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(context).inflate(
+					R.layout.pic_view, parent, false);
 		}
-		ImageView img = (ImageView)convertView.findViewById(R.id.pic);
-		Picasso.with(context).load( feed.getShots().get(position).getImage_teaser_url() ).into( img );
+		ImageView img = (ImageView) convertView.findViewById(R.id.pic);
+		if (hideImages) {
+			int idx = position % colors.length;
+			img.setBackgroundColor(colors[idx]);
+
+		} else {
+			Picasso.with(context)
+					.load(feed.getShots().get(position).getImage_teaser_url())
+					.into(img);
+		}
+
 		return convertView;
 	}
 
@@ -66,7 +82,7 @@ public class DribbbleDataAdapter implements SectionedAdapter {
 
 	@Override
 	public Class[] getViewTypes() {
-		return new Class[]{LinearLayout.class};
+		return new Class[] { LinearLayout.class };
 	}
 
 	@Override
