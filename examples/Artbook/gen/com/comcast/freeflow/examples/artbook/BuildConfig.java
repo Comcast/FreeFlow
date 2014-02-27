e70476da44e3dafb35a66245589860ff7e0351c4
Jake Wharton <jw@squareup.com>
2014-02-27 13:29:41 -0800

    Convert to Gradle build system.

--- examples/Artbook/gen/com/comcast/freeflow/examples/artbook/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.comcast.freeflow.examples.artbook;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
