a171ad7afa3486aa7cfddf8d48da3ec8f915318e
Arpit Mathur <mathur.arpit@gmail.com>
2014-03-14 17:25:54 -0400

    Added a NoAnimationLayoutAnimator class that just jumps between layout changes without animations.

---
+++ FreeFlow/src/com/comcast/freeflow/animations/NoAnimationLayoutAnimator.java
@@ -0,0 +1,47 @@
+package com.comcast.freeflow.animations;
+
+import android.graphics.Rect;
+import android.util.Pair;
+import android.view.View;
+import android.view.View.MeasureSpec;
+
+import com.comcast.freeflow.core.FreeFlowContainer;
+import com.comcast.freeflow.core.FreeFlowItem;
+import com.comcast.freeflow.core.LayoutChangeset;
+
+public class NoAnimationLayoutAnimator implements FreeFlowLayoutAnimator {
+
+	private LayoutChangeset changes;
+	
+	@Override
+	public LayoutChangeset getChangeSet() {
+		return changes;
+	}
+
+	@Override
+	public void cancel() {
+		
+	}
+
+	@Override
+	public void animateChanges(LayoutChangeset changes,
+			FreeFlowContainer callback) {
+		this.changes = changes;
+		for(Pair<FreeFlowItem, Rect> item : changes.getMoved()){
+			Rect r = item.first.frame;
+			View v = item.first.view;
+			int wms = MeasureSpec.makeMeasureSpec(r.right-r.left, MeasureSpec.EXACTLY);
+			int hms = MeasureSpec.makeMeasureSpec(r.bottom-r.top, MeasureSpec.EXACTLY);
+			v.measure(wms,hms );
+			v.layout(r.left, r.top, r.right, r.bottom);	
+		}
+		callback.onLayoutChangeAnimationsCompleted(this);
+		
+	}
+
+	@Override
+	public boolean isRunning() {
+		return false;
+	}
+
+}
